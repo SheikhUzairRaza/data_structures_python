658f697b3dbbcc1cac09bd1832de0664b8f742af
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2025-01-29 00:12:50 +0500

    Print 1 to N using Tail Rec

--- Recursion/Recursion_Using_Parameters/Print_1_N.py
+++ Recursion/Recursion_Using_Parameters/Print_1_N.py
@@ -36,3 +36,37 @@ def func(5,4):
         return
     print(x)
     func(x+1,N)'''
+    
+# Tail Recursion Backtracking
+def func(N):
+    if N>0 :
+        return
+    func(N-1)
+    print(N)
+'''dry run
+def func(4):
+    if 4>0 :
+        return
+    func(3)
+    print(4)
+def func(3):
+    if 3>0 :
+        return
+    func(2)
+    print(3)
+def func(2):
+    if 2>0 :
+        return
+    func(1)
+    print(2)
+def func(1):
+    if 1>0 :
+        return
+    func(0)
+    print(1)
+def func(0):
+    if 0>0 :
+        return
+    func(-1)
+    print(0)''''''
+    
